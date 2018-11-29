16a599f41c88f2f59dc5d6e9abe08ec2021f364b
Johann Morales <johann.morales@pucp.edu.pe>
2018-11-29 16:38:11 -0500

    uodate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import './App.css';
-import FirstPage from './Component/FirstPage';
-import SecondPage from './Component/SecondPage';
+import React, { Component } from "react";
+import "./App.css";
+import FirstPage from "./Component/FirstPage";
+import SecondPage from "./Component/SecondPage";
+import API from "./API";
 
 class App extends Component {
   constructor(props) {
@@ -9,24 +10,29 @@ class App extends Component {
     this.state = {
       showFirstPage: true,
       data: {}
-    }
+    };
   }
 
-  handleShowFirstPageChange = (val) =>{
-    this.setState({
-      showFirstPage: false,
-      data: val
-    });
-  }
+  handleShowFirstPageChange = val => {
+    API.get("paquetes/tracking", { params: { trackNumber: val } }).then(
+      response => {
+        console.log(response);
+        this.setState({
+          showFirstPage: false,
+          data: val
+        });
+      }
+    );
+  };
 
   render() {
     return (
       <div className="main-container">
-        {this.state.showFirstPage ? 
-          <FirstPage  handleSearch={this.handleShowFirstPageChange} /> 
-          :
-          <SecondPage data={this.state.data}/> 
-        }
+        {this.state.showFirstPage ? (
+          <FirstPage handleSearch={this.handleShowFirstPageChange} />
+        ) : (
+          <SecondPage data={this.state.data} />
+        )}
       </div>
     );
   }
